2a68585a0cefa45f37e901daf5a311c04dd052ac
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-08-14 17:53:34 +0200

    Allow user to update options for posting.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update_attributes!(user_params)
+    flash[:success] = 'Your changes were saved.'
+    redirect_to user_path
+  end
+
+  def user_params
+    params.require(:user).permit(:posting_from_mastodon, :boost_options, :masto_reply_options, :masto_mention_options)
   end
 end
