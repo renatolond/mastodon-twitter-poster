26486e952b8fe5061c8db27196ab40994c3d3c76
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-08-17 10:09:50 +0200

    Get short url length from twitter api, exit when missing credentials

--- daemons/check_for_toots.rb
+++ daemons/check_for_toots.rb
@@ -174,4 +174,18 @@ Signal.trap("INT") {
 
 Rails.logger.debug { "Starting" }
 
+twitter_client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['TWITTER_CLIENT_ID']
+  config.consumer_secret = ENV['TWITTER_CLIENT_SECRET']
+end
+
+begin
+  TootTransformer::twitter_short_url_length = twitter_client.configuration.short_url_length
+  TootTransformer::twitter_short_url_length_https = twitter_client.configuration.short_url_length_https
+rescue Twitter::Error::Forbidden => ex
+  Rails.logger.error { "Missing Twitter credentials" }
+  exit
+end
+
+
 CheckForToots::available_since_last_check
