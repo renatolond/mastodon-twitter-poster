8fb48ffd735f8500dc7ce40a507c00a5f5357365
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-08-25 18:36:06 +0200

    Remove broken unused tests for controllers

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,4 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get users_update_url
-    assert_response :success
-  end
-
 end
