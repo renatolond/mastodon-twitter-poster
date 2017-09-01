ca1415aac3e82b3c1105c4239ef26b2d843264d1
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-09-01 13:50:29 +0200

    Fix locale issue, add locale guessing.

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data'
+
+gem "http_accept_language", "~> 2.1"
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,2 @@
-I18n.available_locales = [:pt, :en]
-I18n.default_locale = :en
+Rails.application.config.i18n.available_locales = [:pt, :en]
+Rails.application.config.i18n.default_locale = :en
