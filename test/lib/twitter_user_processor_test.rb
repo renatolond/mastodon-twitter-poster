b587e67ad2eb9a1f48a003b2627c7bd5c1fb50e9
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-09-04 19:59:38 +0300

    Remove unnecessary param to fake object

--- test/lib/twitter_user_processor_test.rb
+++ test/lib/twitter_user_processor_test.rb
@@ -78,7 +78,7 @@ class TwitterUserProcessorTest < ActiveSupport::TestCase
   end
 
   test 'process_tweet - retweet' do
-    user = create(:user_with_mastodon_and_twitter, twitter_last_check: 6.days.ago)
+    user = create(:user_with_mastodon_and_twitter)
 
     stub_request(:get, 'https://api.twitter.com/1.1/statuses/show/904738384861700096.json').to_return(web_fixture('twitter_retweet.json'))
 
@@ -90,7 +90,7 @@ class TwitterUserProcessorTest < ActiveSupport::TestCase
   end
 
   test 'process tweet - manual retweet' do
-    user = create(:user_with_mastodon_and_twitter, twitter_last_check: 6.days.ago)
+    user = create(:user_with_mastodon_and_twitter)
 
     stub_request(:get, 'https://api.twitter.com/1.1/statuses/show/895311375546888192.json').to_return(web_fixture('twitter_manual_retweet.json'))
 
@@ -102,7 +102,7 @@ class TwitterUserProcessorTest < ActiveSupport::TestCase
   end
 
   test 'process tweet - reply to tweet' do
-    user = create(:user_with_mastodon_and_twitter, twitter_last_check: 6.days.ago)
+    user = create(:user_with_mastodon_and_twitter)
 
     stub_request(:get, 'https://api.twitter.com/1.1/statuses/show/904746849814360065.json').to_return(web_fixture('twitter_reply.json'))
 
@@ -113,7 +113,7 @@ class TwitterUserProcessorTest < ActiveSupport::TestCase
     TwitterUserProcessor::process_tweet(t, user)
   end
   test 'process tweet - reply to user' do
-    user = create(:user_with_mastodon_and_twitter, twitter_last_check: 6.days.ago)
+    user = create(:user_with_mastodon_and_twitter)
 
     stub_request(:get, 'https://api.twitter.com/1.1/statuses/show/904747662070734848.json').to_return(web_fixture('twitter_mention.json'))
 
