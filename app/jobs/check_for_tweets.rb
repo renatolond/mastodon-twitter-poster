e8e64a42e3f0009deec52e84b3ace8c73d0e3965
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-09-04 20:33:40 +0300

    Add check_for_tweets daemon

--- app/jobs/check_for_tweets.rb
+++
@@ -1,7 +0,0 @@
-class CheckForTweets
-  OLDER_THAN_IN_SECONDS = 30
-  def self.available_since_last_check
-    u = User.where('twitter_last_check < now() - interval \'? seconds\'', OLDER_THAN_IN_SECONDS).order(twitter_last_check: :asc).first
-    return unless u.twitter
-  end
-end
--- daemons/check_for_toots.rb
+++ daemons/check_for_toots.rb
@@ -3,18 +3,7 @@ require 'mastodon_ext'
 require 'mastodon_user_processor'
 require 'toot_transformer'
 require 'httparty'
-
-class InterruptibleSleep
-  def sleep(seconds)
-    @_sleep_check, @_sleep_interrupt = IO.pipe
-    IO.select([@_sleep_check], nil, nil, seconds)
-  end
-
-  def wakeup
-    @_sleep_interrupt.close if @_sleep_interrupt && !@_sleep_interrupt.closed?
-  end
-end
-
+require 'interruptible_sleep'
 
 class CheckForToots
   OLDER_THAN_IN_SECONDS = 30
