5426b8912759c8ab1cf5160d37435769ba7cddbf
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-10-26 13:41:14 +0200

    Add regression test for #10.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -203,4 +203,29 @@ class UserTest < ActiveSupport::TestCase
       user.save!
     end
   end
+  test 'User changes domain and toot counter is not reset' do
+    user = create(:user_with_mastodon_and_twitter, last_toot: 20010010)
+
+    authorization = build(:authorization_mastodon, uid: user.mastodon.uid, token: 'super-crazy-token')
+    auth = mock()
+    credentials = mock()
+    auth.expects(:provider).at_least(1).returns(authorization.provider)
+    auth.expects(:uid).at_least(1).returns(authorization.uid)
+    auth.expects(:credentials).at_least(1).returns(credentials)
+    credentials.expects(:token).returns(authorization.token)
+    credentials.expects(:secret).returns(authorization.secret)
+
+    mastodon_id = 123
+    expected_mastodon_status_id = 2
+    mastodon_client = mock()
+    user.expects(:mastodon_client).returns(mastodon_client)
+    user.expects(:mastodon_id).returns(mastodon_id)
+    mastodon_status = mock()
+    mastodon_client.expects(:statuses).with(mastodon_id, {limit: 1}).returns([mastodon_status])
+    mastodon_status.expects(:id).returns(expected_mastodon_status_id)
+
+    u = User.from_omniauth(auth, user)
+    assert_not_equal 20010010, u.last_toot
+    assert_equal expected_mastodon_status_id, u.last_toot
+  end
 end
