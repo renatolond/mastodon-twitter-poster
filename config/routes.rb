e6305a8b347f235441920bbf16e185765bada7be
Renato "Lond" Cerqueira <renato@lond.com.br>
2017-11-23 21:24:34 +0100

    Add a logout button to the top menu. At the moment there's no way to connect multiple accounts. But it should be possible to connect multiple pairs of accounts just by logging out and connecting a new account. Related to #18.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :authorizations, only: [:destroy]
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   resource :user
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
