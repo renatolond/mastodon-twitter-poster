2470d3ac610a658cf343965859a093ef5c7baec7
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-05-04 11:49:05 +0200

    Add privacy selection to option form and crossposting status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def mastodon?
     user_signed_in? && !current_user.mastodon.nil?
   end
+
+  def translated_masto_privacy_for_select(class_name, enum)
+    class_name.send(enum.to_s.pluralize).map do |key, _|
+      [I18n.t("masto_privacy.#{key}"), key]
+    end
+  end
 end
