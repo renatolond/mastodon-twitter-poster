6d9583ef7972822c5643e79a2dac06c54752e4d6
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-06-10 13:01:42 +0200

    Remove unused DYNO variable This variable was provided by heroku when the project first started.

--- lib/stats.rb
+++ lib/stats.rb
@@ -7,10 +7,6 @@ class Stats
     @statsd = Statsd.new('127.0.0.1', 8125)
   end
 
-  def dyno_id
-    @dyno_id ||= ENV['DYNO'] || 'unknown'
-  end
-
   def increment(name)
     @statsd.increment("#{name}")
   end
