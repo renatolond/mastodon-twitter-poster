6dd22a1c4fa2ea114e43f52da55bab5afbdf9f13
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-09-17 20:39:14 +0200

    Improve @ detection

--- lib/mastodon_user_processor.rb
+++ lib/mastodon_user_processor.rb
@@ -282,7 +282,7 @@ class MastodonUserProcessor
 
   def tweet(content, opts = {})
     Rails.logger.debug { "Posting to twitter: #{content}" }
-    raise 'Contains @' if content.gsub(toot.url, '').match?(/[^A-Za-z]@/)
+    raise 'Contains @' if content.gsub(toot.url, '').gsub(/https:\/\/[^\s\/]+\/@[^\s\/]+\/[0-9]+/, '').gsub('@ ', '').gsub(/@\Z/, '').match?(/[^A-Za-z0-9]@＠/)
     begin
       status = user.twitter_client.update(content, opts)
     rescue Twitter::Error::Forbidden => ex
