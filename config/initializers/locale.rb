57d64dcbcac922227944597299db92fe7b593cef
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-09-17 20:19:10 +0200

    Add de, sort languages alphabetically

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,2 @@
-Rails.application.config.i18n.available_locales = [:ar, :'pt-BR', :pt, :pl, :en, :nl]
+Rails.application.config.i18n.available_locales = [:ar, :de, :en, :nl, :pl, :'pt-BR', :pt]
 Rails.application.config.i18n.default_locale = :en
