7ff9737a5fc0cf4c080f137976583fb22c1c8248
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-09-19 19:18:39 +0200

    Add Norwegian Bokmål

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,2 @@
-Rails.application.config.i18n.available_locales = [:ar, :de, :en, :nl, :pl, :'pt-BR', :pt]
+Rails.application.config.i18n.available_locales = [:ar, :de, :en, :'nb-NO', :nl, :pl, :'pt-BR', :pt]
 Rails.application.config.i18n.default_locale = :en
