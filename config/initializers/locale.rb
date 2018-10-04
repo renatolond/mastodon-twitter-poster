af2d08c481dadd60d75b464ea35f4464e33e8d29
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-10-04 07:14:53 +0200

    Add spanish translation

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,2 @@
-Rails.application.config.i18n.available_locales = [:ar, :de, :en, :'nb_NO', :nl, :pl, :'pt-BR', :pt]
+Rails.application.config.i18n.available_locales = [:ar, :de, :en, :es, :'nb_NO', :nl, :pl, :'pt-BR', :pt]
 Rails.application.config.i18n.default_locale = :en
