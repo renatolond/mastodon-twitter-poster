f1dab140e867a574617e98b250ce4bc0efcb5aa7
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-11-16 08:48:58 +0100

    Refactor stats, add test

--- app/objects/stats.rb
+++ app/objects/stats.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
-if ENV['STATSD_ENABLED'] == 'true'
-  class Stats
-    def initialize
-      @statsd = Statsd.new(ENV['STATSD_HOST'], ENV['STATSD_PORT'])
-    end
+class Stats
+  def self.enabled?
+    @@enabled = ENV["STATSD_ENABLED"] == "true"
+  end
 
-    def increment(name)
-      @statsd.increment("#{name}")
-    end
-
-    def timing(name, ms)
-      @statsd.timing(name, ms)
-    end
-
-    def time(name, &blk)
-      @statsd.time(name, &blk)
+  def initialize
+    if self.class.enabled?
+      @statsd = Statsd.new(ENV["STATSD_HOST"], ENV["STATSD_PORT"])
     end
   end
-else
-  class Stats
-    def increment(name); end
-    def timing(name, ms); end
-    def time(name, &blk); end
+
+  def increment(name)
+    @statsd&.increment("#{name}")
+  end
+
+  def timing(name, ms)
+    @statsd&.timing(name, ms)
+  end
+
+  def time(name, &blk)
+    @statsd&.time(name, &blk)
   end
 end
---
+++ test/objects/stats_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class StatsTest < ActiveSupport::TestCase
+  test "With statsd enabled, call increment calls statsd" do
+    statsd_mock = mock()
+    statsd_mock.expects(:increment)
+    Statsd.expects(:new).returns(statsd_mock)
+
+    Stats.expects(:enabled?).returns(true)
+
+    Stats.new.increment("test")
+  end
+
+  test "With statsd disabled, call increment and nothing happens" do
+    statsd_mock = mock()
+    statsd_mock.expects(:increment).never
+
+    Statsd.expects(:new).never.returns(statsd_mock)
+
+    Stats.expects(:enabled?).returns(false)
+
+    Stats.new.increment("test")
+  end
+end
