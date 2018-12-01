5d81eca80d66165524dea3254c5d9c50f90c8248
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-12-01 20:09:21 +0100

    Add bulma-rtl to fully support RTL languages

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( application-rtl.css )
 #
