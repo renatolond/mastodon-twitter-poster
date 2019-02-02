a2996af826c712057bd7c8921c4b5d0ea9c9fcee
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-02-02 15:55:05 +0100

    Add sidekiq route if user is admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   resource :account
   resources :authorizations, only: [:destroy]
 
