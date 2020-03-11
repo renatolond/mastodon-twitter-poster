93bb041d3e11a09ff45bbe615e1f50aca37c5e37
Gavin Mogan <git@gavinmogan.com>
2020-03-11 00:58:19 -0700

    Make optional configs truely optional

--- config/initializers/crossposter_config.rb
+++ config/initializers/crossposter_config.rb
@@ -2,18 +2,18 @@
 
 Rails.application.configure do
   config.x.application_name = ENV.fetch("CROSSPOSTER_APP_NAME", "Mastodon Twitter Crossposter")
-  config.x.domain = ENV.fetch("CROSSPOSTER_DOMAIN")
+  config.x.domain = ENV.fetch("CROSSPOSTER_DOMAIN", nil)
   config.x.repo = ENV.fetch("CROSSPOSTER_REPO", "https://github.com/renatolond/mastodon-twitter-poster")
 
   config.x.stats = ENV.fetch("CROSSPOSTER_STATS", nil)
 
-  config.x.announcement_account_address = ENV.fetch("CROSSPOSTER_FEDI_ACCOUNT_ADDRESS")
-  config.x.announcement_account_at = ENV.fetch("CROSSPOSTER_FEDI_ACCOUNT_AT")
+  config.x.announcement_account_address = ENV.fetch("CROSSPOSTER_FEDI_ACCOUNT_ADDRESS", nil)
+  config.x.announcement_account_at = ENV.fetch("CROSSPOSTER_FEDI_ACCOUNT_AT", nil)
 
   config.x.admin_twitter = ENV.fetch("CROSSPOSTER_ADMIN_TWITTER", nil)
 
-  config.x.admin_fedi_address = ENV.fetch("CROSSPOSTER_ADMIN_FEDI_ADDRESS")
-  config.x.admin_fedi_at = ENV.fetch("CROSSPOSTER_ADMIN_FEDI_AT")
+  config.x.admin_fedi_address = ENV.fetch("CROSSPOSTER_ADMIN_FEDI_ADDRESS", nil)
+  config.x.admin_fedi_at = ENV.fetch("CROSSPOSTER_ADMIN_FEDI_AT", nil)
 
   config.x.use_alternative_twitter_domain = ActiveModel::Type::Boolean.new.cast(ENV.fetch("USE_ALTERNATIVE_TWITTER_DOMAIN", "FALSE"))
   config.x.alternative_twitter_domain = ENV.fetch("ALTERNATIVE_TWITTER_DOMAIN", "twitter.activitypub.actor")
