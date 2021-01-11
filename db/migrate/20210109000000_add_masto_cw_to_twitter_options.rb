ac12eb633f3ef4f9ce4bfdbe902a8233ad3b3206
David Baumgold <david@davidbaumgold.com>
2021-01-11 12:04:07 +0100

    update migration

--- db/migrate/20210109000000_add_masto_cw_to_twitter_options.rb
+++ db/migrate/20210109000000_add_masto_cw_to_twitter_options.rb
@@ -3,6 +3,6 @@ class AddMastoCwToTwitterOptions < ActiveRecord::Migration[5.1]
     execute <<-SQL
       CREATE TYPE masto_cw_options AS ENUM ('CW_AND_CONTENT', 'CONTENT_ONLY', 'CW_ONLY');
     SQL
-    add_column :users, :masto_cw_options, :masto_cw_options
+    add_column :users, :masto_cw_options, :masto_cw_options, null: false, default: "CW_ONLY"
   end
 end
