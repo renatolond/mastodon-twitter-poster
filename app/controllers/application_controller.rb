4670382f68792544a295c6c380863948b6aa2d6d
Renato "Lond" Cerqueira <renato@lond.com.br>
2021-01-13 14:54:47 +0100

    Turn set locale into an around_action
    Avoid setting the language to other requests too.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_locale
+  around_action :set_locale
 
-  def set_locale
-    I18n.locale = params[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+  def set_locale(&action)
+    locale = params[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 end
