0ea568ce85981fe226b385698ce64013793bc2c7
Renato "Lond" Cerqueira <renato@lond.com.br>
2021-01-13 14:50:38 +0100

    Add new locales

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,2 @@
-Rails.application.config.i18n.available_locales = [:ar, :cs, :de, :en, :es, :fr, :id, :it, :'nb_NO', :nl, :pl, :'pt-BR', :pt, :'zh_Hant']
+Rails.application.config.i18n.available_locales = %i[ar ca cs de en eo es fa fr ia id it nb_NO nl pl pt pt-BR ru zh_Hant].freeze
 Rails.application.config.i18n.default_locale = :en
