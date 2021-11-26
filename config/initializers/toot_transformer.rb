3918826355729a41e6daee464aabc122939f7a45
Renato "Lond" Cerqueira <renato@lond.com.br>
2021-11-26 10:26:29 +0100

    Use static values for twitter config

--- config/initializers/toot_transformer.rb
+++ config/initializers/toot_transformer.rb
@@ -1,19 +1,8 @@
-if Rails.env.test? || File.split($0).last == "rake"
-  Rails.logger.warn { "Using hardcoded values for twitter url length" }
-  TootTransformer.twitter_short_url_length = 23
-  TootTransformer.twitter_short_url_length_https = 23
-else
-  twitter_client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV["TWITTER_CLIENT_ID"]
-    config.consumer_secret = ENV["TWITTER_CLIENT_SECRET"]
-  end
+# frozen_string_literal: true
 
-  begin
-    twitter_config = twitter_client.configuration
-    TootTransformer.twitter_short_url_length = twitter_config.short_url_length
-    TootTransformer.twitter_short_url_length_https = twitter_config.short_url_length_https
-  rescue Twitter::Error::Forbidden, Twitter::Error::BadRequest
-    Rails.logger.error { "Missing Twitter credentials" }
-    exit 1
-  end
-end
+# These values are constants on Twitter's side and used
+# to come from an endpoint for configuration. It is no
+# longer the case (https://twittercommunity.com/t/retiring-the-1-1-configuration-endpoint/153319)
+# if those values change, they need to be changed here.
+TootTransformer.twitter_short_url_length = 23
+TootTransformer.twitter_short_url_length_https = 23
