90de85e2d8d471b696e82bb894aece279360a038
Renato "Lond" Cerqueira <renato@lond.com.br>
2022-07-19 00:19:29 +0200

    Avoid scheduling users twice in sidekiq
    This changes the scheduler to avoid inserting the same user twice, by
    trying to reduce the amount of data loaded in memory and doing
    operations directly in SQL.

--- app/objects/scheduler/check_for_statuses_scheduler.rb
+++ app/objects/scheduler/check_for_statuses_scheduler.rb
@@ -8,9 +8,22 @@ class Scheduler::CheckForStatusesScheduler
   OLDER_THAN_IN_SECONDS = 30
 
   def perform
-    User.where("locked = ? AND (posting_from_mastodon = ? OR posting_from_twitter = ?) AND (mastodon_last_check < now() - interval '? seconds' or twitter_last_check < now() - interval '? seconds')", false, true, true, OLDER_THAN_IN_SECONDS, OLDER_THAN_IN_SECONDS).order(mastodon_last_check: :asc, twitter_last_check: :asc).each do |user|
-      user.locked = true; user.save
-      ProcessUserWorker.perform_async(user.id)
+    # The crossposter was duplicating jobs in the queue and the issue seems to be that this method sometime takes a long time.
+    # There's a lock_ttl with sidekiq unique jobs, but this also makes it so that in the case that fails, no users should be inserted twice.
+    # This is achived by first selecting all possible user ids, then iterating through them, selecting the user for update (to lock the row),
+    # if the user at this point was locked, we skip the user_id.
+    # Otherwise, we update the user_id and add the user to the queue.
+    #
+    # The update_all there is to avoid having to load the AR object in memory, since we just locked the row.
+    user_ids = User.where("locked = ? AND (posting_from_mastodon = ? OR posting_from_twitter = ?) AND (mastodon_last_check < now() - interval '? seconds' or twitter_last_check < now() - interval '? seconds')", false, true, true, OLDER_THAN_IN_SECONDS, OLDER_THAN_IN_SECONDS).order(mastodon_last_check: :asc, twitter_last_check: :asc).pluck(:id)
+    user_ids.each do |user_id|
+      User.transaction do
+        locked = User.where(id: user_id).lock!.pick(:locked)
+        next if locked
+
+        User.where(id: user_id).update_all(locked: true) # rubocop:disable Rails/SkipsModelValidations
+        ProcessUserWorker.perform_async(user_id)
+      end
     end
   end
 end
