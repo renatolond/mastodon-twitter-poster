6bd7e8c8648037d169fdf82cb2b8615092ea1ba3
Renato "Lond" Cerqueira <renato@lond.com.br>
2022-12-28 18:55:04 +0100

    Make it easier to disconnect accounts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @uid = current_user.mastodon&.uid
+      @domain = current_user.mastodon_client&.base_url
       @handle = current_user.twitter_handle
       # TODO: Merge this Stoplight with the one in MastodonUserProcessor to avoid different params
       @stoplight_domain_status = Stoplight("source:#{current_user.mastodon_domain}").with_threshold(3).with_cool_off_time(5.minutes.seconds).color if current_user.mastodon
